675d614e694069ba8c5fb3c634cfb4992a953d98
Sourodeep Guin <imsourodeepguin@gmail.com>
2026-08-16 12:33:44 +0530

    Create 69-Sqrtx

---
+++ 69-Sqrtx/69-Sqrtx.py
@@ -0,0 +1,10 @@
+# Last updated: 16/08/2026, 12:33:43
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0: return 0
+        for i in range(1,x+1):
+            if i*i == x:
+                return i
+            elif i*i > x:
+                return i-1
+        
