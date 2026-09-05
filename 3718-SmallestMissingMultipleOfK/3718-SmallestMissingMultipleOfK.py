0fc63e242562bf25f5cc5f414907c6d5bb1f06cb
Sourodeep Guin <imsourodeepguin@gmail.com>
2026-09-05 12:51:44 +0530

    Create 3718-SmallestMissingMultipleOfK

---
+++ 3718-SmallestMissingMultipleOfK/3718-SmallestMissingMultipleOfK.py
@@ -0,0 +1,9 @@
+# Last updated: 05/09/2026, 12:51:44
+class Solution:
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        i = 1
+        while (True):
+            if (k*i) not in nums:
+                return k*i
+
+            i+=1
