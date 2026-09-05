8db50a9e07837bf106c51af7830a94aee5063a00
Sourodeep Guin <imsourodeepguin@gmail.com>
2026-09-05 12:52:01 +0530

    Create 1470-ShuffleTheArray

---
+++ 1470-ShuffleTheArray/1470-ShuffleTheArray.py
@@ -0,0 +1,9 @@
+# Last updated: 05/09/2026, 12:52:01
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        ans =[]
+
+        for i in range(n):
+            ans.append(nums[i])
+            ans.append(nums[i+n])
+        return ans
